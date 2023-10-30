ae929837907697069d6ce3cb3519c8410cd519f3
Tiskas02 <111374293+Tiskas02@users.noreply.github.com>
2023-10-30 23:31:40 +0700

    Update Studenttracking.js

--- INT201/week09/Studenttracking.js
+++ INT201/week09/Studenttracking.js
@@ -28,35 +28,33 @@ const students = [
   //let studentAttendence = students[0]
   //let studentAttendanceTime = studentAttendence.attendance
   function getAttendanceRate(student) {
-      let totalattendance = 0;
-      let attendances = 0;
-        for (const attendance of student.attendance) {
-            if (attendance){
-              attendances++
-            }
-            totalattendance++
-        }
-      let averageAttendance = (attendances / totalattendance)*100
-  return averageAttendance
-      }
+      const stu = student.filter((truee) => truee == true)
+     
+      return (stu.length / student.length )*100
+  }
+  // const atdRate = student.filter((student) => student == true) // [true,true,true]
+  // return (atdRate.length / student.length) * 100
+
+  
       
 
   function getAvgScore(student){
-    let totalScore = 0;
-    let count = 0;
-    for (const score of student.testScores) {
-      totalScore += score;
-      count++;
-    }
-      const averageScore = totalScore / count;
-      return averageScore
+    return student.reduce((acc,cur) => (acc + cur)/5,0)
       // averageScores.push(averageScore);
     }
   // }
 
- let avgScore= getAvgScore(students[0]);
- console.log(avgScore);
 
 function underPerformStudent(student){
-  
+  return student.filter((el) => getAttendanceRate(el.attendance) < 80 && getAvgScore(el.testScores) < 70)
+  .map((el2) =>{
+    return {
+    name : el2.name,
+    attendanceRate : getAttendanceRate(el2.attendance),
+    AvgScore : getAvgScore(el2.testScores)
+    }
+  }
+  )
 }
+
+console.log(underPerformStudent(students));
